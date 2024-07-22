eb6435de078d40572b13c8b3d78e94cb836131a6
firealarmss <ko4uyj@gmail.com>
2024-07-21 23:14:36 -0500

    Refactor to be an electron app

--- index.js
+++ index.js
@@ -1,37 +1,75 @@
-const express = require('express');
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const express = require('express');
 const fs = require('fs');
 const yaml = require('js-yaml');
-const app = express();
 
-const codeplugDirectory = path.join(__dirname, 'codeplugs');
-
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
-
-function loadCodeplugs() {
-    const files = fs.readdirSync(codeplugDirectory);
-    const codeplugs = files.map(file => {
-        const filepath = path.join(codeplugDirectory, file);
-        const codeplug = yaml.load(fs.readFileSync(filepath, 'utf8'));
-        return {
-            name: path.basename(file, path.extname(file)),
-            data: codeplug
-        };
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+            enableRemoteModule: false,
+            nodeIntegration: true
+        }
     });
-    return codeplugs;
+
+    win.loadURL('http://localhost:3000');
 }
 
-app.get('/', (req, res) => {
-    const codeplugs = loadCodeplugs();
-    const selectedCodeplug = codeplugs[0]; // Default to the first codeplug
-    res.render('index', { codeplugs, selectedCodeplug });
+function startServer() {
+    const app = express();
+    const codeplugDirectory = path.join(__dirname, 'codeplugs');
+
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, 'views'));
+
+    app.set('view engine', 'ejs');
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    function loadCodeplugs() {
+        const files = fs.readdirSync(codeplugDirectory);
+        const codeplugs = files.map(file => {
+            const filepath = path.join(codeplugDirectory, file);
+            const codeplug = yaml.load(fs.readFileSync(filepath, 'utf8'));
+            return {
+                name: path.basename(file, path.extname(file)),
+                data: codeplug
+            };
+        });
+        return codeplugs;
+    }
+
+    app.get('/', (req, res) => {
+        const codeplugs = loadCodeplugs();
+        const selectedCodeplug = codeplugs[0];
+        res.render('index', { codeplugs, selectedCodeplug });
+    });
+
+    app.get('/codeplug/:name', (req, res) => {
+        const codeplugs = loadCodeplugs();
+        const selectedCodeplug = codeplugs.find(cp => cp.name === req.params.name);
+        res.render('index', { codeplugs, selectedCodeplug });
+    });
+
+    app.listen(3000, () => console.log(`Server running on port 3000`));
+}
+
+app.on('ready', () => {
+    startServer();
+    createWindow();
 });
 
-app.get('/codeplug/:name', (req, res) => {
-    const codeplugs = loadCodeplugs();
-    const selectedCodeplug = codeplugs.find(cp => cp.name === req.params.name);
-    res.render('index', { codeplugs, selectedCodeplug });
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
-app.listen(3000, () => console.log(`Server running on port 3000`));
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
